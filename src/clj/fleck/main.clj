9b9d95089da1a749df61f6d953b6abe65bbaae54
mike <mike706574@gmail.com>
2017-01-30 23:17:45 -0600

    Commit.

--- src/clj/fleck/main.clj
+++ src/clj/fleck/main.clj
@@ -1,15 +1,14 @@
 (ns fleck.main
   (:require [com.stuartsierra.component :as component]
+            [environ.core :refer [env]]
             [fleck.system :as system])
   (:gen-class :main true))
 
 (def id "fleck")
-(def port 8080)
-(def config {:id id :port port})
 
 (defn -main
-  [& args]
-  (if (= (count args) 0)
-    (do (component/start-system (system/system config))
-        @(promise))
-    (println (str "Usage: java -jar " id ".jar server"))))
+  [& [port]]
+  (let [port (or port (env :port) 5000)]
+    (component/start-system
+     (system/system {:id "fleck" :port port}))
+    @(promise)))
