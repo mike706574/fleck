2564b29fadb4bff38bad44d179e61d602159b3f3
mike <mike706574@gmail.com>
2017-01-30 23:20:59 -0600

    Log port. Make it an integer.

--- src/clj/fleck/main.clj
+++ src/clj/fleck/main.clj
@@ -1,6 +1,7 @@
 (ns fleck.main
   (:require [com.stuartsierra.component :as component]
             [environ.core :refer [env]]
+            [taoensso.timbre :as log]
             [fleck.system :as system])
   (:gen-class :main true))
 
@@ -8,7 +9,8 @@
 
 (defn -main
   [& [port]]
-  (let [port (or port (env :port) 5000)]
+  (let [port (Integer. (or port (env :port) 5000))]
+    (log/info (str "Using port " port "."))
     (component/start-system
      (system/system {:id "fleck" :port port}))
     @(promise)))
